01d39f33f4f0dfbbd3135f2c6d50abc96e23ace3
hypnoai <52698628+hypnoai@users.noreply.github.com>
2019-07-18 11:55:30 +0100

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ angular.module('app', [])
   var vm = this;
   vm.message = 'Hello world';
   vm.title = "Hypno.AI";
-  vm.desc = "Bringing the web to hypnosis."
+  vm.desc = "Bringing the web to hypnosis.";
+  vm.contactAddress = "v@hypno.ai";
 }]);
 
 setTimeout(function() {
